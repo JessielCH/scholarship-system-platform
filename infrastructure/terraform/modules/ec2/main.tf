988d43c3cf0f0cffc1b8eb7be2edb8f74b9d3bbc
Jessiel <jessieljosue@gmail.com>
2026-05-29 15:39:19 -0500

    fix(ci): sync package-lock and format terraform files

--- infrastructure/terraform/modules/ec2/main.tf
+++ infrastructure/terraform/modules/ec2/main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "this" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  
+
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = var.key_name
 
