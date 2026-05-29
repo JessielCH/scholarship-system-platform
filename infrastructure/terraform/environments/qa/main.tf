deb26f62bd06fa77c37690513ddd2ef789e9d9e8
Jessiel <jessieljosue@gmail.com>
2026-05-29 15:04:59 -0500

    feat(infra): add EC2 module and test instance in QA

--- infrastructure/terraform/environments/qa/main.tf
+++ infrastructure/terraform/environments/qa/main.tf
@@ -7,3 +7,47 @@ module "vpc" {
   private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
   availability_zones   = ["us-east-1a", "us-east-1b"]
 }
+
+resource "aws_security_group" "ec2_basic" {
+  name        = "qa-ec2-basic-sg"
+  description = "Allow SSH and ICMP for testing"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ICMP from anywhere"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "qa-ec2-basic-sg"
+    Environment = "qa"
+  }
+}
+
+module "test_ec2" {
+  source = "../../modules/ec2"
+
+  environment        = "qa"
+  name               = "test-node"
+  subnet_id          = module.vpc.public_subnet_ids[0]
+  security_group_ids = [aws_security_group.ec2_basic.id]
+  # instance_type and key_name usan valores por defecto (t2.micro y vockey)
+}
