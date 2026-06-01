b9a981b060a19c9781946832f222be7cd55d508e
Jessiel <jessieljosue@gmail.com>
2026-06-01 06:47:09 -0500

    fix(infra): add second AZ to QA and Dev VPCs for RDS subnet group requirement

--- infrastructure/terraform/environments/develop/main.tf
+++ infrastructure/terraform/environments/develop/main.tf
@@ -3,9 +3,9 @@ module "vpc" {
 
   environment          = "dev"
   vpc_cidr             = "10.1.0.0/16"
-  public_subnet_cidrs  = ["10.1.1.0/24"]
-  private_subnet_cidrs = ["10.1.11.0/24"]
-  availability_zones   = ["us-east-1a"]
+  public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnet_cidrs = ["10.1.11.0/24", "10.1.12.0/24"]
+  availability_zones   = ["us-east-1a", "us-east-1b"]
 }
 
 module "ec2_test" {
--- infrastructure/terraform/environments/qa/main.tf
+++ infrastructure/terraform/environments/qa/main.tf
@@ -3,9 +3,9 @@ module "vpc" {
 
   environment          = "qa"
   vpc_cidr             = "10.2.0.0/16"
-  public_subnet_cidrs  = ["10.2.1.0/24"]
-  private_subnet_cidrs = ["10.2.11.0/24"]
-  availability_zones   = ["us-east-1a"]
+  public_subnet_cidrs  = ["10.2.1.0/24", "10.2.2.0/24"]
+  private_subnet_cidrs = ["10.2.11.0/24", "10.2.12.0/24"]
+  availability_zones   = ["us-east-1a", "us-east-1b"]
 }
 
 module "ec2_qa" {
