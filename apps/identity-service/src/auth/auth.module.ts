9b380de2e7ede5cf79761f25210b06040e291ca2
Jessiel <jessieljosue@gmail.com>
2026-06-02 14:11:36 -0500

    feat(auth): implement real database authentication using TypeORM and bcrypt

--- apps/identity-service/src/auth/auth.module.ts
+++ apps/identity-service/src/auth/auth.module.ts
@@ -3,8 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 
+import { User } from '../users/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'super-secret-key-for-qa',
       signOptions: { algorithm: 'HS256' },
