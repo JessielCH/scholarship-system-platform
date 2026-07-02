4860f9df2785d911b2814e226d4cc19a5eeb2e31
JessielCH <jjchasiguano@uce.edu.ec>
2026-07-02 14:33:54 -0500

    feat(saga): setup workflow saga state machine
    Resolves SCHOL-27, SCHOL-82, SCHOL-83, SCHOL-84

---
+++ apps/workflow-saga/src/main/java/com/uce/saga/WorkflowSagaApplication.java
@@ -0,0 +1,12 @@
+package com.uce.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowSagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowSagaApplication.class, args);
+    }
+}
