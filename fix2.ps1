0d65a41f8049f583989ce3f791457cefa3d3ea10
JessielCH <jjchasiguano@uce.edu.ec>
2026-07-07 04:57:28 -0500

    chore: remove temporary diagnostic scripts

--- fix2.ps1
+++
@@ -1,1 +0,0 @@
-ssh -i user_key.pem -o StrictHostKeyChecking=no ubuntu@35.172.67.236 "docker exec scholarship_gateway sed -i \"s/rewritePrefix: x27x27/rewritePrefix: ''/g\" /app/dist/index.js && docker restart scholarship_gateway"
