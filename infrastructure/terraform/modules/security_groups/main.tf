99a69e744e6e1bb0cb9672bb66bc851b5080aceb
JessielCH <jjchasiguano@uce.edu.ec>
2026-07-25 08:56:26 -0500

    fix(infra): open database ports in security groups

--- infrastructure/terraform/modules/security_groups/main.tf
+++ infrastructure/terraform/modules/security_groups/main.tf
@@ -236,7 +236,13 @@ resource "aws_security_group" "database" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.compute.id, aws_security_group.edge.id]
+    security_groups = [
+      aws_security_group.edge.id,
+      aws_security_group.core.id,
+      aws_security_group.security.id,
+      aws_security_group.compute.id,
+      aws_security_group.intelligence.id
+    ]
   }
 
   egress {
